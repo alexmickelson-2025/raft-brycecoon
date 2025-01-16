b3726c872d61cedeb6cbc29ea6185d8912216489
brycecoon <112597142+brycecoon@users.noreply.github.com>
2025-01-16 11:17:07 -0700

    all tests passing

--- DistributedTDDProject1/Node.cs
+++ DistributedTDDProject1/Node.cs
@@ -16,9 +16,13 @@ public class Node
     public int term;
     public nodeState state; //starts as the first option (follower)
     public Node[] neighbors;
-    public System.Timers.Timer timer;
+    public System.Timers.Timer electionTimeoutTimer;
+    public System.Timers.Timer heartbeatTimer;
     public long timeoutInterval;
     public Guid currentLeader;
+    public int numVotesRecieved;
+
+    public int heartbeatsSent = 0; //for testing
 
     public Node()
     {
@@ -27,24 +31,43 @@ public class Node
         voteTerm = 0;
         timeoutInterval = Random.Shared.NextInt64(150,301);
         neighbors = [];
-        timer = new System.Timers.Timer(timeoutInterval);
-        timer.Elapsed += Timer_Timeout;
-        timer.AutoReset = false;
-        timer.Start();
+        electionTimeoutTimer = new System.Timers.Timer(timeoutInterval);
+        electionTimeoutTimer.Elapsed += Timer_Timeout;
+        electionTimeoutTimer.AutoReset = false;
+        electionTimeoutTimer.Start();
     }
 
     public void setElectionResults()
     {
         double voteCount = 0;
         if (voteId == id) { voteCount++; }
-        foreach (Node node in neighbors)
-        {
-            if (node.voteId == id) { voteCount++; }
-        }
-        if (voteCount >= Math.Ceiling(((double)neighbors.Length+1) / 2))
+        if (numVotesRecieved >= Math.Ceiling(((double)neighbors.Length+1) / 2))
         {
             state = nodeState.LEADER;
+            StartHeartbeat();
         }
+    }
+
+    private void StartHeartbeat()
+    {
+        foreach(Node node in neighbors)
+        {
+            sendAppendRPC(node);
+        }
+        heartbeatsSent++;
+        heartbeatTimer = new System.Timers.Timer(50);
+        heartbeatTimer.Elapsed += sendHeartbeat;
+        heartbeatTimer.AutoReset = true;
+        heartbeatTimer.Start();
+    }
+
+    private void sendHeartbeat(object sender, ElapsedEventArgs e)
+    {
+        foreach(Node node in neighbors)
+        {
+            sendAppendRPC(node);
+        }
+        heartbeatsSent++;
     }
 
     public void requestVote(Node[] nodes)
@@ -55,8 +78,14 @@ public class Node
             {
                 node.voteId = id;
                 node.voteTerm = term;
+                sendYesVote();
             }
         }
+    }
+
+    private void sendYesVote()
+    {
+        numVotesRecieved++;
     }
 
     public string sendAppendRPC(Node recievingNode)
@@ -68,8 +97,8 @@ public class Node
         if (recievingNode.term <= term)
         {
             recievingNode.currentLeader = id;
-        return "recieved";
-
+            recievingNode.ResetTimer();
+            return "recieved";
         }
         else { return "rejected"; }
     }
@@ -77,22 +106,24 @@ public class Node
     public void startElection()
     {
         voteId = id;
+        sendYesVote();
         term++;
         state = nodeState.CANDIDATE;
     }
 
     public void Timer_Timeout(object sender, ElapsedEventArgs e)
     {
-        ResetTimer();
         startElection();
+        ResetTimer();
     }
 
     public void ResetTimer()
     {
+        electionTimeoutTimer.Stop();
         timeoutInterval = Random.Shared.NextInt64(150, 301);
-        timer = new System.Timers.Timer(timeoutInterval);
-        timer.Elapsed += Timer_Timeout;
-        timer.AutoReset = false;
-        timer.Start();
+        electionTimeoutTimer = new System.Timers.Timer(timeoutInterval);
+        electionTimeoutTimer.Elapsed += Timer_Timeout;
+        electionTimeoutTimer.AutoReset = false;
+        electionTimeoutTimer.Start();
     }
 }
