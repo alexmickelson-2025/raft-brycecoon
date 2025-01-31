91575368b388cd6986ed05e7733b6daa28377a29
brycecoon <112597142+brycecoon@users.noreply.github.com>
2025-01-31 11:19:56 -0700

    fixed version

--- DockerWebApp/Program.cs
+++ DockerWebApp/Program.cs
@@ -1,8 +1,6 @@
 using System.Text.Json;
 using DistributedTDDProject1;
-using OpenTelemetry.Logs;
-using OpenTelemetry.Resources;
- 
+
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://0.0.0.0:8080");
